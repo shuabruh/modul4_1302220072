f238ed0837aff31ddfc37bfa0ff502411abd6a4e
shuabruh <shuabruh@telkomuniversity.ac.id>
2024-03-14 11:12:01 +0700

    Commit akhir

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿public enum BUAH
+﻿using System.Runtime.CompilerServices;
+
+public enum BUAH
 {
     APEL,
     APRIKOT,
@@ -37,10 +39,65 @@ public class KodeBuah
     }
 }
 
+public class PosisiKarakter
+{
+    private POSISI awal;
+
+    public PosisiKarakter()
+    {
+        awal = POSISI.BERDIRI;
+    }
+
+    TriggerKarakter[] transisi =
+    {
+            new TriggerKarakter(POSISI.JONGKOK, POSISI.BERDIRI, TRIGGER_POS.TOMBOL_W),
+            new TriggerKarakter(POSISI.BERDIRI, POSISI.TERBANG, TRIGGER_POS.TOMBOL_W),
+            new TriggerKarakter(POSISI.TENGKURAP, POSISI.JONGKOK, TRIGGER_POS.TOMBOL_W),
+            new TriggerKarakter(POSISI.JONGKOK, POSISI.TENGKURAP, TRIGGER_POS.TOMBOL_S),
+            new TriggerKarakter(POSISI.BERDIRI, POSISI.JONGKOK, TRIGGER_POS.TOMBOL_S),
+            new TriggerKarakter(POSISI.TERBANG, POSISI.BERDIRI, TRIGGER_POS.TOMBOL_S),
+            new TriggerKarakter(POSISI.TERBANG, POSISI.JONGKOK, TRIGGER_POS.TOMBOL_X)
+    };
+
+    public POSISI getNextPos(POSISI posAwal, TRIGGER_POS trigger)
+    {
+        POSISI posAkhir = posAwal;
+        for(int i = 0; i < transisi.Length; i++)
+        {
+            TriggerKarakter perubahan = transisi[i];
+            if(posAwal == perubahan.posAwal && trigger == perubahan.posTrigger)
+            {
+                posAkhir = perubahan.posAkhir;
+            }
+        }
+        return posAkhir;
+    }
+
+    public void aktivasi(TRIGGER_POS triggerPos)
+    {
+        awal = getNextPos(awal, triggerPos);
+        awal = getNextPos(awal, triggerPos);
+    }
+}
+
+public class TriggerKarakter
+{
+    public POSISI posAwal;
+    public POSISI posAkhir;
+    public TRIGGER_POS posTrigger;
+
+    public TriggerKarakter(POSISI posAwal, POSISI posAkhir, TRIGGER_POS posTrigger)
+    {
+        this.posAwal = posAwal;
+        this.posAkhir = posAkhir;
+        this.posTrigger = posTrigger;
+    }
+}
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
     }
 }
